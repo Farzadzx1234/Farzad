91740422d0e23294dcf11065e994524cf3a545b0
Dark_Night <s.mohammadjavad85@gmail.com>
2022-12-30 23:14:24 +0330

    Add all files

---
+++ assets/script/olink.js
@@ -0,0 +1,10 @@
+
+let linkdata = {
+  1 : "https://discord.gg/HJsKfr7MBW",
+  2 : "https://top.gg/bot/866598611387154433"
+}
+
+function openLink(numlink){
+  let no = numlink;
+  window.open(linkdata[no], '_blank', 'location=yes,height=650,width=1150,scrollbars=yes,status=yes');
+}
